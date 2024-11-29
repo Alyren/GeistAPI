88a948cc4b7d36f32fd6a4ba780d1248950626cd
Arylen <177881981+Alyren@users.noreply.github.com>
2024-11-29 01:48:20 -0600

    Add projects

---
+++ GeistTools.API/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace GeistTools.API
+{
+    [BepInPlugin("com.arylen.geist.api", "GeistTools API", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"API is loaded!");
+        }
+    }
+}
---
+++ GeistTools.Tweaks/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace GeistTools.Tweaks
+{
+    [BepInPlugin("com.arylen.geist.tweaks", "GeistTools Tweaks", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"Tweaks is loaded!");
+        }
+    }
+}
