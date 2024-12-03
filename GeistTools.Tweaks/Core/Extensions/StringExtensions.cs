acaab75f06870ab51b5c6c739bedb161a6001593
Alyren <177881981+Alyren@users.noreply.github.com>
2024-12-03 00:39:58 -0600

    Add StringExtensions

---
+++ GeistTools.Tweaks/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeistTools.Tweaks.Core.Extensions
+{
+    internal static class StringExtensions
+    {
+        public static string CensorWindowsUsername(this string value)
+        {
+            var username = Environment.UserName;
+            var censoredName = "".PadLeft(username.Length, '*');
+            return value.Replace(username, censoredName);
+        }
+    }
+}
